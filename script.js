4a4fe4a6f221edbb9ad73b4c7b6a872e1b573175
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-08-17 22:12:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function threeSum(arr, target) {
 // write your code here
-  
-}
+	let sum=0;
+	let dif=0;
+	arr.sort();
+	for(i=0;i<arr.length;i++){
+		j=i+1;
+		k=arr.length-1;
+
+		while(j<k){
+			if(arr[i]+arr[j]+arr[k]<target){
+				if(dif<Math.abs(arr[i]+arr[j]+arr[k]-target)){
+					sum=arr[i]+arr[j]+arr[k];
+				}
+				j++;
+			}
+			else{
+				if(dif<Math.abs(arr[i]+arr[j]+arr[k]-target)){
+					sum=arr[i]+arr[j]+arr[k];
+				}
+				k--;
+			}
+		}
+	}
+		return sum;
+	}
 
 module.exports = threeSum;
