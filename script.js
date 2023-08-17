605e77c47ab4a98406afef56f76cf2b6daf049a9
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-08-17 22:24:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 function threeSum(arr, target) {
-// write your code here
-	let sum=0;
-	let dif=0;
-	arr.sort();
-	for(let i=0;i<arr.length;i++){
-		let j=i+1;
-		let k=arr.length-1;
+    let sum = 0;
+    let dif = Infinity; // Initialize the difference with positive infinity
+    arr.sort((a, b) => a - b); // Sort the array in ascending order
 
-		while(j<k){
-			if(arr[i]+arr[j]+arr[k]<target){
-				if(dif<Math.abs(arr[i]+arr[j]+arr[k]-target)){
-					sum=arr[i]+arr[j]+arr[k];
-				}
-				j++;
-			}
-			else{
-				k--;
-			}
-		}
-	}
-		return sum;
-	}
+    for (let i = 0; i < arr.length; i++) {
+        let j = i + 1;
+        let k = arr.length - 1;
+
+        while (j < k) {
+            let currentSum = arr[i] + arr[j] + arr[k];
+            let currentDiff = Math.abs(currentSum - target);
+
+            if (currentDiff < dif) {
+                dif = currentDiff;
+                sum = currentSum;
+            }
+
+            if (currentSum < target) {
+                j++;
+            } else {
+                k--;
+            }
+        }
+    }
+    return sum;
+}
+
 
 module.exports = threeSum;
